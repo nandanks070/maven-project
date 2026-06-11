3ea995bea0766b69886f5e5dac1a5952680518d6
nandanks070 <nandanks070@gmail.com>
2026-06-11 11:27:51 +0530

    Add main application class

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+/**
+ * Hello world application
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Maven Project!");
+    }
+
+    public static String greet(String name) {
+        return "Hello, " + name + "!";
+    }
+}
